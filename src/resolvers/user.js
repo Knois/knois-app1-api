a9c0fb6777192f5cef0ab089b79dd913190c4f0d
Knois <knoisknois@gmail.com>
2022-01-19 17:15:07 +0300

    Fix

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,10 +1,12 @@
 module.exports = {
   // При запросе разрешается список заметок автора
   notes: async (user, args, { models }) => {
-    return await models.Note.find({ author: user._id }).sort({ _id: -1 });
+    return await models.Note.find({ author: user._id }).sort({ updatedAt: -1 });
   },
   // При запросе разрешается список избранных заметок
   favorites: async (user, args, { models }) => {
-    return await models.Note.find({ favoritedBy: user._id }).sort({ _id: -1 });
+    return await models.Note.find({ favoritedBy: user._id }).sort({
+      updatedAt: -1
+    });
   }
 };
